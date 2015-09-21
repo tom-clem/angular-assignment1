1783559dc828db4f836b9f27403ff68b82b34177
tom-clem <t_h_o_m_a_s@live.at>
2015-09-21 11:18:17 -0600

    Running System
    Data from KSmith which is now running for Reps

--- client/js/app.js
+++ client/js/app.js
@@ -1,77 +1,59 @@
 
-//alert('hello world');
-
-
-angular.module('RepsApp', [
-
+angular.module('SearchApp', [
   'RepsAppControllers'
 ]);
 
+angular
+  .module('RepsAppControllers', [
+    'repsService'
+  ])
+  .controller('MainCtrl', function (reps) {
+    var main2 = this;
+    main2.reps = [];
 
+    main2.searchByZip = function (zip) {
+      reps.allByZip(zip).then(function (data) {
+        main2.reps = data;
+      });
+    };
 
-angular.module('RepsAppControllers',[
-  'repsService'
-])
-.controller('MainCrtl', function(reps){
-  this.foo = 'hello world'; //Variable deklarieren - wird hier nicht gebraucht
+    main2.searchRepsByName = function (name) {
+      reps.repsByName(name).then(function (data) {
+        main2.reps = data;
+      });
+    };
 
-  var main = this; //By using var we can use the variable also within other functions
-  main.reps =[];
-  main.sens =[];
-
-
-
-
-/*  $http
-  .get('http://dgm-representatives.herokuapp.com/reps/by-name/Smith')
-  .then(function(res){ // res = response
-    main.reps = res.data;
-  //console.log(res);
-  }); */
-
-  main.searchRepsByZip = function (zip){
-    reps.allByZip(zip).then(function(data){
-      main.reps = data;
-    });
-  };
-
-  main.searchRepsByName = function (name){
-    reps.repsByName(name).then(function(data){
-      main.reps = data;
-    });
-  };
-  main.searchRepsByState = function (state){
-    reps.repsBySate(state).then(function(data){
-      main.reps = data;
-    });
-  };
-});
+    main2.searchRepsByState = function (state) {
+      reps.repsByState(state).then(function (data) {
+        main2.reps = data;
+      });
+    };
+  });
 
 angular
-.module('repsService', [])
-.factory('reps', function ($http){
-  var host = 'http://dgm-representatives.herokuapp.com';
-  return {
-    allByZip: function(zip){
-      return $http
-      .get(host +'/all/by-zip/' + zip)
-      .then(function(response){
-        return response.data;
-    });
-  },
-  repsByName: function(name){
-    return $http
-    .get(host +'/reps/by-name/' + name)
-    .then(function(response){
-      return response.data;
+  .module('repsService', [])
+  .factory('reps', function ($http) {
+    var host = 'http://dgm-representatives.herokuapp.com';
+    return {
+      allByZip: function (zip) {
+        return $http
+          .get(host + '/all/by-zip/' + zip)
+          .then(function (response) {
+            return response.data;
+          });
+      },
+      repsByName: function (name) {
+        return $http
+          .get(host + '/reps/by-name/' + name)
+          .then(function (response) {
+            return response.data;
+          });
+      },
+      repsByState: function (state) {
+        return $http
+          .get(host + '/reps/by-state/' + state)
+          .then(function (response) {
+            return response.data;
+          });
+    }};
   });
-},
-repsBySate: function(state){
-  return $http
-  .get(host +'/reps/by-state/' + state)
-  .then(function(response){
-    return response.data;
-});
-}
-  };
-});
